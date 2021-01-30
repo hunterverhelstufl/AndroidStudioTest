303250ef0bfce8d52af8b650a88ae26cf22e296a
Hunter Ver Helst <73560351+hunterverhelstufl@users.noreply.github.com>
2021-01-29 22:57:04 -0500

    Adding main files to repository

---
+++ app/src/main/java/com/example/test/ui/main/MainFragment.java
@@ -0,0 +1,52 @@
+package com.example.test.ui.main;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.test.R;
+
+public class MainFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    public void buttonClickHandler(View view) {
+        try {
+            Button b = (Button) view;
+            b.setText("I Was just clicked");
+            Log.d("Success", "Button clicked");
+        }
+        catch (Exception e) {
+            Log.d("Not Successful", "not a button");
+        }
+    }
+
+}
